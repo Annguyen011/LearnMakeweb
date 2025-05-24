340dba96cd4b5b8b4adcb1fb956e0d18d8ebb4df
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-05-24 23:23:26 +0700

    Fix script loading and update demo text ID in Lesson 1

--- Lession 1/js/script.js
+++ Lession 1/js/script.js
@@ -1,6 +1,6 @@
 console.log("Chào mừng đến với Lesson 1!");
 const updateTextStyle = () => {
-    const textElement = document.getElementById("demoText");
+    const textElement = document.getElementById("Demo-text");
     textElement.style.color = "blue";
     textElement.style.fontSize = "20px";
     textElement.style.fontWeight = "bold";
