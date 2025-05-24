7d93293a02c5bd9dff7bfb9da0f45b3ea0085452
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-05-24 23:20:52 +0700

    Add demo text styling functionality to Lesson 1

--- Lession 1/js/script.js
+++ Lession 1/js/script.js
@@ -1,1 +1,7 @@
 console.log("Chào mừng đến với Lesson 1!");
+const updateTextStyle = () => {
+    const textElement = document.getElementById("demoText");
+    textElement.style.color = "blue";
+    textElement.style.fontSize = "20px";
+    textElement.style.fontWeight = "bold";
+}
